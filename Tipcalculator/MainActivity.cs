678507b75ce7445a9a8349418f5bfc249aef656d
Esmerlin Rosario <merlin.moya@gmail.com>
2017-03-10 19:04:55 -0400

    Initial check-in of module Tipcalculator

---
+++ Tipcalculator/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace Tipcalculator
+{
+	[Activity(Label = "Tipcalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		EditText inputBill;
+		Button calculateButton;
+		TextView outputTip;
+		TextView outputTotal;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+
+			inputBill = FindViewById<EditText>(Resource.Id.InputBill);
+			calculateButton = FindViewById<Button>(Resource.Id.CalculatorButton);
+			outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+			outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+			calculateButton.Click += OnCalculateClick;
+
+		}
+
+		void OnCalculateClick(object sender, EventArgs e)
+		{
+			string text = inputBill.Text;
+			var bill = double.Parse(text);
+			var tip = bill * 0.15;
+			var total = bill + tip;
+			outputTip.Text = tip.ToString();
+			outputTotal.Text = total.ToString();
+		}
+	}
+}
